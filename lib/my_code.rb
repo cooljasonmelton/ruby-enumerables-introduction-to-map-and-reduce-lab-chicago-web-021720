ebe4ee902d0d04bb9fc7bd2bb26565734a0b0877
Jason Melton <jason.melton2@gmail.com>
2020-01-24 21:54:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,10 @@ def map_to_negativize(source_array)
   array = []
   i = 0 
   while i < source_array.length do 
-    source_array[i]
+    array << (source_array[i] * -1)
+    i += 1 
+  end 
+  array 
 end 
 
 def map_to_no_change(source_array)

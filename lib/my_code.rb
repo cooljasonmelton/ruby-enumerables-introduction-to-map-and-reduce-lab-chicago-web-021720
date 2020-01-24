423108d0bdeae0f0c6b5069be1dd4d4b8b860ed0
Jason Melton <jason.melton2@gmail.com>
2020-01-24 22:01:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,7 +39,7 @@ def map_to_square(source_array)
   array 
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   total = 
   i = 0 
   while i < source_array
